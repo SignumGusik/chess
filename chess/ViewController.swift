fc69dd7e6c7d7f1ca2bd3051a7888c2fe9d1dcb5
SignumGusik <darya.gus@internet.ru>
2026-03-31 19:01:20 +0300

    Initial Commit

---
+++ chess/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chess
+//
+//  Created by Диана on 31/03/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ chessTests/chessTests.swift
@@ -0,0 +1,17 @@
+//
+//  chessTests.swift
+//  chessTests
+//
+//  Created by Диана on 31/03/2026.
+//
+
+import Testing
+@testable import chess
+
+struct chessTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
